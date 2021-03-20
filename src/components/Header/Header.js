9e2df4ebb5d00d02f321615661b22405a928d020
misha-l <mkrusharska@gmail.com>
2021-03-20 06:41:27 -0700

    menu links selection

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,23 +2,78 @@ import React from "react";
 import "./Header.scss";
 import { FaRegUserCircle } from "react-icons/fa";
 
+class Header extends React.Component {
+  /*
+  className={
+    window.location.pathname === "/Judges" ? "selected" : ""
+  }
+  */
+  state = {
+    links: [
+      {
+        url: "/",
+        caption: "Начало",
+        selected: false,
+      },
+      {
+        url: "/Judges",
+        caption: "Жури",
+        selected: false,
+      },
+      {
+        url: "/Gallery",
+        caption: "Галерия",
+        selected: false,
+      },
+      {
+        url: "/Finalists",
+        caption: "Финалисти",
+        selected: false,
+      },
+      {
+        url: "/Pravila",
+        caption: "Правила",
+        selected: false,
+      },
+    ],
+  };
 
-const Header = () => {
-  return (
-    <div className="header">
-      <div className="login"><a href="#"><FaRegUserCircle /><b>Вход</b></a>
+  componentDidMount() {
+    const selectedLinks = this.state.links.map((link) =>
+      link.url === window.location.pathname ? { ...link, selected: true } : link
+    );
+    this.setState({ links: selectedLinks });
+  }
+
+  renderlHeaderLinks() {
+    const linksHtml = this.state.links.map((link) => {
+      return (
+        <li key={link.url}>
+          <a href={link.url} className={link.selected ? "selected" : ""}>
+            {link.caption} {link.selected}
+          </a>
+        </li>
+      );
+    });
+    return <ul>{linksHtml}</ul>;
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <div className="login">
+          <a href="/sdfsdfsfd">
+            <FaRegUserCircle />
+            <b>Вход</b>
+          </a>
+        </div>
+        {this.renderlHeaderLinks()}
+        <span>
+          <a href="/sdfswrwsdfsdf">Изпрати Рисунка</a>
+        </span>
       </div>
-      <ul>
-        <li><a href="/">Начало</a></li>
-        <li><a href="/Judges">Жури</a></li>
-        <li><a href="/Gallery">Галерия</a></li>
-        <li><a href="/Finalists">Финалисти</a></li>
-        <li><a href="/Pravila">Правила</a></li>
-      </ul>
-      
-      <span><a href="">Изпрати Рисунка</a></span>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
